b54704ac5d411903e1632633393bb72ace85e9b9
al036991 <al036991@ieslaencanta.com>
2022-03-28 10:11:02 +0200

    terminado

--- Ahorcado/src/es/rgmf/Main.java
+++ Ahorcado/src/es/rgmf/Main.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Locale;
 import java.util.Scanner;
 
 public class Main {
@@ -11,17 +12,18 @@ public class Main {
     public static void main(String[] args) {
         String frase;
         String[] frases;
+        String solucion = "";
         char[] fraseOculta;
         Scanner entrada = new Scanner(System.in);
         Path path;
 
         // Carga el fichero con las frases
-        path = Paths.get("src/es/rgmf/frases.txt");
+        path = Paths.get("Ahorcado/src/es/rgmf/frases.txt");
         try {
             frases = Files.lines(path).toArray(String[]::new);
         } catch (IOException e) {
             e.printStackTrace();
-            frases = new String[] {"Hola Amigo"};
+            frases = new String[]{"Hola Amigo"};
         }
 
         // Prepara la frase
@@ -41,6 +43,35 @@ public class Main {
         System.out.println(fraseOculta);
 
         // TODO Completa el programa.
+
+        char opcion;
+        do {
+            System.out.print("Dame una letra: ");
+            char letra = entrada.nextLine().charAt(0);
+            System.out.println();
+
+            // "Levantamos" la letra en la frase oculta.
+            for (int i = 0; i < frase.length(); i++) {
+                if (frase.toLowerCase().charAt(i) == letra || frase.toUpperCase().charAt(i) == letra) {
+                    fraseOculta[i] = frase.charAt(i);
+                }
+            }
+
+            System.out.println(fraseOculta);
+
+            System.out.print("¿Quieres resolver la frase? [S/N] ");
+            opcion = entrada.nextLine().charAt(0);
+
+        } while (opcion != 'S' && opcion != 's');
+
+        System.out.print("Intenta resolver la frase: ");
+        solucion = entrada.nextLine();
+
+        if (solucion.equalsIgnoreCase(frase)) {
+            System.out.println("Has adivinado la frase");
+        } else {
+            System.out.println("No has acertado, la frase era: " + frase);
+        }
 
         entrada.close();
     }
